0e981f0e479821458fe6fb58cd8f24388a76e553
saileshkhadka <saileshkhadka1@gmail.com>
2023-12-26 22:24:49 +0400

    error handeling for mime-type

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,17 @@ import App from './App.jsx'
 import './index.css'
 
 
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
