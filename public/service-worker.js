3e7a592e8325ee29c2cf261e4193f4b76c59a9ff
saileshkhadka <saileshkhadka1@gmail.com>
2023-12-27 21:43:24 +0400

    code changes on load issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,16 @@
-self.addEventListener('fetch', (event) => {
-    const responseHeaders = new Headers({
-      'X-Content-Type-Options': 'nosniff',
-      // Add other headers if needed
-    });
+// self.addEventListener('fetch', (event) => {
+//     const responseHeaders = new Headers({
+//       'X-Content-Type-Options': 'nosniff',
+//       // Add other headers if needed
+//     });
   
-    event.respondWith(
-      fetch(event.request, { headers: responseHeaders })
-        .then((response) => {
-          return response;
-        })
-        .catch((error) => {
-          console.error('Error fetching:', error);
-        })
-    );
-  });
+//     event.respondWith(
+//       fetch(event.request, { headers: responseHeaders })
+//         .then((response) => {
+//           return response;
+//         })
+//         .catch((error) => {
+//           console.error('Error fetching:', error);
+//         })
+//     );
+//   });
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import './index.css'
 
 
 // Register the service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/service-worker.js')
+//     .then((registration) => {
+//       console.log('Service Worker registered with scope:', registration.scope);
+//     })
+//     .catch((error) => {
+//       console.error('Service Worker registration failed:', error);
+//     });
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
